0f7ce79cf704d99cb3f42301adeed3e2be6fbc98
fsitter42 <fsitter@42vienna.com>
2026-02-11 16:01:03 +0100

    splitt_added

--- sandbox/readline.c
+++ sandbox/readline.c
@@ -1,4 +1,5 @@
-#include "../shinimell/minishell.h"
+#include "../minishell/minishell.h"
+#include "libft.h"
 
 void printh(void)
 {
@@ -28,13 +29,22 @@ int count_pipes(char *prompt)
 
 int handle_pipes(char *prompt, int pipe_count)
 {
-		while (1)
-			return (0);
+		int i = 0;
+		int index = 1;
+		char **pipes = ft_split(prompt, '|');
+		if (!pipes)
+			return (1);
+		while (pipes[i])
+		{
+			printf("Pipe %i, %s\n",index + i, pipes[i]);
+			i++;
+		}
+		return (0);
 }
 
 int main()
 {
-	int pipes;	
+	int x_pipes;	
 	while (1)
 	{
     	char *prompt = readline("minishell$ ");
@@ -45,10 +55,10 @@ int main()
 				break;
 		}
 		add_history(prompt);
-		pipes = count_pipes(prompt);
+		x_pipes = count_pipes(prompt);
+		handle_pipes(prompt, x_pipes);
     	free(prompt);
     	prompt = NULL;
-    	printf("%i\n", pipes);
 	}
 	printh();
 	rl_clear_history();

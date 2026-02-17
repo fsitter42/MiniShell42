69b50410d7280e4d13d5b3d4645393d08ee3e8cb
fsitter42 <fsitter@42vienna.com>
2026-02-17 12:49:34 +0100

    pipe two way handshake

--- sandbox/pipehandshake_two_way.c
+++ sandbox/pipehandshake_two_way.c
@@ -20,13 +20,19 @@ int main()
         close(p_2_c[1]); // closes the write
         close(c_2_p[0]); // close the read
 
-
         printf("Child: Waiting for parent.\n");
-
         read(p_2_c[0], &dummy, 1);
-
         printf("Child: Parent is done.. starting now\n");
-        close(p_2_c[0]);
+        sleep(1);
+        printf("Child: Im done..\n");
+        close(c_2_p[1]);
+        printf("Child: Waiting for parent.\n");
+        read(p_2_c[0], &dummy, 1);
+        printf("Child: thank you for telling me.\n");
+        
+
+
+        close(p_2_c[0]); // close read chanel
         sleep(2);
         exit(0);
     }
@@ -38,11 +44,21 @@ int main()
         printf("Parent: Im doing someting important the child needs.\n");
         sleep(2);
         printf("Parent: Im done. Lets go child.\n");
+        write(p_2_c[1], "1", 1);
 
-        close(p_2_c[1]);
+        printf("Parent: Im now waiting for my child to be ready.\n");
+        read(c_2_p[0], &dummy, 1);
+        printf("Parent: Ok ill start now.\n");
+        sleep(1);
+        printf("Parent: Done lets go home.\n");
+        close(p_2_c[1]); // 2nd use
         waitpid(pid, NULL, 0);
         printf("Parent: Child is done.\n");
     }
     return (0);
 }
 
+
+// using write instead of close lets me reuse the pipe because
+// close sends EOF
+// write sends whatever i send

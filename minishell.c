722f16eeb6dd32e51dc12f3774624d6353929682
Bert Burt <slambert@student.42vienna.com>
2026-02-23 12:35:24 +0100

    basic tokenization implemented via singly linked list

--- minishell.c
+++ minishell.c
@@ -6,11 +6,12 @@
 /*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 10:48:22 by slambert          #+#    #+#             */
-/*   Updated: 2026/02/22 17:11:21 by slambert         ###   ########.fr       */
+/*   Updated: 2026/02/23 12:34:37 by slambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minishell.h"
+#include <stdio.h>
 
 /*
 	input parsing - Stefan
@@ -45,23 +46,81 @@
 	-
 	*/
 
-/*  we want to split the line into certain tokens.
-	possible tokens are:
-	- WORD
-	- PIPE (|)
-	- REDIR_IN (<)
-	- REDIR_OUT (>)
-	- REDIR_APPEND (>>)
-	- HEREDOC (<<)
-
-*/
-
 int		is_part_of_word(char c);
 
-void	tokenizer(char *line)
-{
-	int	i;
+/* this is where the magic happens, same in debug and in normal mode
+	tokenizer(line);
+	syntax check , if negative return error msg and return prompt
+	if positive, continue
+	create list of structs that are relevant for execution.
+	this includes:
+	- expansion
+	- quotes
+	- list of cmds
+	free(line);
+	pass this list to execute part of program (frido)
+*/
 
+// this function does everything that is needed that ONE LINE is being executed correctly
+int	loop(char *line)
+{
+	t_token	*cur_elem;
+
+	// 1. tokenizer
+	printf("'%s' is going to be tokenized\n", line);
+	cur_elem = NULL;
+	cur_elem = tokenizer(line);
+	free(line);
+}
+
+void	init_token(t_token *token)
+{
+	token->type = START;
+	token->str = NULL;
+	token->status = WIP;
+	token->next = NULL;
+}
+
+void	tokenlist_add(t_token *list_start, int type, char *str)
+{
+	t_token	*new_token;
+
+	new_token = ft_calloc(1, sizeof(t_token));
+	// if (!new_token)
+	// error handling
+	while (list_start->next)
+		list_start = list_start->next;
+	list_start->next = new_token;
+	init_token(new_token);
+	new_token->type = type;
+	new_token->str = str;
+}
+
+/* we want to structure the input and save it in a linked list. in order to know what elements
+   occur, we have to do certain checks to the string. these tokens are possible:
+   - PIPE: |
+   - HEREDOC: <<
+   - REDIR_IN: <
+   - REDIR_APPEND: >>
+   - REDIR_OUT: >
+   - WORD
+   - END
+   return: NULL on empty line or a pointer to the first element
+*/
+t_token	*tokenizer(char *line)
+{
+	t_token	*list_start;
+	int		i;
+	int		word_start;
+	char	*word = NULL;
+
+	// TODO difference between these 2 cases
+	if (!line || ft_strncmp(line, "", 1) == 0)
+		return (NULL);
+	list_start = ft_calloc(1, sizeof(t_token));
+	// if (!list_start)
+	// error handling
+	init_token(list_start);
 	i = -1;
 	while (line[++i])
 	{
@@ -70,36 +129,48 @@ void	tokenizer(char *line)
 		if (line[i] == '|')
 		{
 			printf("PIPE ");
+			tokenlist_add(list_start, PIPE, NULL);
 			continue ;
 		}
 		if (line[i] == '<' && line[i + 1] == '<')
 		{
 			printf("HEREDOC ");
+			tokenlist_add(list_start, HEREDOC, NULL);
 			i++;
 			continue ;
 		}
 		if (line[i] == '<')
 		{
 			printf("REDIR_IN ");
+			tokenlist_add(list_start, REDIR_IN, NULL);
 			continue ;
 		}
 		if (line[i] == '>' && line[i + 1] == '>')
 		{
 			printf("REDIR_APPEND ");
+			tokenlist_add(list_start, REDIR_APPEND, NULL);			
 			i++;
 			continue ;
 		}
 		if (line[i] == '>')
 		{
 			printf("REDIR_OUT ");
+			tokenlist_add(list_start, REDIR_OUT, NULL);			
 			continue ;
 		}
 		// WORD
+		word_start = i;
 		while (is_part_of_word(line[i]))
 			i++;
+		i--;
+		word = ft_strdup(ft_substr(line, word_start, i - word_start + 1));
+		//if (!word)
+		//error hanlding
+		tokenlist_add(list_start, WORD, word);
 		printf("WORD ");
 	}
 	printf("\n");
+	return list_start;
 }
 
 int	is_part_of_word(char c)
@@ -109,18 +180,8 @@ int	is_part_of_word(char c)
 	return (1);
 }
 
-/* this is where the magic happens, same in debug and in normal mode
-*/
-int loop (char *line)
-{
-    //1. tokenizer
-	printf("'%s' is going to be tokenized\n", line);
-    tokenizer(line);
-    free(line);
-}
-
 /* this is the default mode in where the users enters stuff
-*/
+ */
 int	normal_mode(int argc, char **argv, char **envp)
 {
 	char	*line;
@@ -132,51 +193,43 @@ int	normal_mode(int argc, char **argv, char **envp)
 			return (-1);
 		if (!strcmp(line, ""))
 			add_history((const char *)line);
-		tokenizer(line);
-		// syntax check , if negative return error msg and return prompt
-		// if positive, continue
-		// create list of structs that are relevant for execution.
-		// this includes:
-		// - expansion
-		// - quotes
-		// - list of cmds
-		free(line);
-		// pass this list to execute part of program (frido)
+		loop(line);
 	}
 }
 
 /* the difference here is that we have one string that is given to
-*  minishell, no interactions with the user. all lines have to be separated
-*  by a semicolon (;) and will be executed one after each other.
-*/
-int    debug_mode(char *input, char **envp)
+ *  minishell, no interactions with the user. all lines have to be separated
+ *  by a semicolon (;) and will be executed one after each other.
+ */
+int	debug_mode(char *input, char **envp)
 {
-    char **strs = NULL;
-    int i;
-    
-    strs = ft_split (input, ';');
-    if (!strs)
-        exit(EXIT_FAILURE);
-    i = -1;
-    while (strs[++i])
-        loop(strs[i]);
+	char	**strs;
+	int		i;
+
+	strs = NULL;
+	strs = ft_split(input, ';');
+	if (!strs)
+		exit(EXIT_FAILURE);
+	i = -1;
+	while (strs[++i])
+		loop(strs[i]);
 }
 
-/* we can start minishell in normal (user input) mode (argc = 1) or in 
+/* we can start minishell in normal (user input) mode (argc = 1) or in
  * debug mode (with the -d flag followed by a string) - argc = 3
-*/
+ */
 int	main(int argc, char **argv, char **envp)
 {
-    //if the program was compiled with the -d flag and argc is 2
-    // we run minishell in debug mode, otherwise normal mode
-    if (argc != 1 && argc != 3)
-        return (printf("wrong syntax - argc not 1 or 3\n"), 1);
-    if (argc == 1)
-        normal_mode(argc, argv, envp);
-    else
-    {
-        if (ft_strncmp(argv[1], "-d", 2) != 0)
-            return (printf("wrong syntax - did you use the -d flag?\n"), 1);
-        debug_mode(argv[2], envp);
-    }
+	// if the program was compiled with the -d flag and argc is 2
+	// we run minishell in debug mode, otherwise normal mode
+	if (argc != 1 && argc != 3)
+		return (printf("wrong syntax - argc not 1 or 3\n"), 1);
+	if (argc == 1)
+		normal_mode(argc, argv, envp);
+	else
+	{
+		if (ft_strncmp(argv[1], "-d", 2) != 0)
+			return (printf("wrong syntax - did you use the -d flag?\n"), 1);
+		debug_mode(argv[2], envp);
+	}
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 01:38:55 by slambert          #+#    #+#             */
-/*   Updated: 2026/02/22 15:12:04 by slambert         ###   ########.fr       */
+/*   Updated: 2026/02/23 12:28:10 by slambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,31 @@
 
 typedef struct s_token
 {
-	// TODO aus type ein enum machen
-	int		type;
-	// wird standardmäßig auf NULL gesetzt und nur bei einem WORD befüllt
-	char	*value;
-    struct s_token *next;
-}			t_token;
+	int type; // enum
+	char			*str;
+	int				status;
+	struct s_token	*next;
+	//struct s_token	*previous;
 
-void		tokenizer(char *line);
+}					t_token;
+
+// enums
+enum				e_token_types
+{
+	START = 0,
+	WORD,
+	VAR,
+	PIPE,
+	HEREDOC,
+	REDIR_IN,
+	REDIR_APPEND,
+	REDIR_OUT,
+};
+
+enum	e_token_status
+{
+	WIP = 0,
+	DONE
+};
+
+t_token*				tokenizer(char *line);

57232575831dc4f694cb446e7e7c736a3b4fe9c2
Bert Burt <slambert@student.42vienna.com>
2026-03-02 16:40:44 +0100

    expansion is very tedious

--- tokenizer/expansion.c
+++ tokenizer/expansion.c
@@ -6,7 +6,7 @@
 /*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 12:33:32 by slambert          #+#    #+#             */
-/*   Updated: 2026/03/02 15:04:51 by slambert         ###   ########.fr       */
+/*   Updated: 2026/03/02 16:40:14 by slambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,21 @@ char *extract_var_from_envp(char *var_name, char **envp)
 {
     char *value;
     int i;
-    int len_var;
-    int len_one_elem;
+    int len_var_name;
+    int len_total;
     
+   // var_name
     i = -1;
-    len_var = ft_strlen(var_name);
+    len_var_name = ft_strlen(var_name);
     while (envp[++i])
     {
-        if (ft_strncmp(envp[i], var_name, len_var) == 0)
+        if (ft_strncmp(envp[i], var_name, len_var_name) == 0)
         {
-            len_one_elem = ft_strlen(envp [i]);
-            return ft_substr(envp[i], len_var + 1, len_one_elem - len_var + 1);
+            len_total = ft_strlen(envp [i]);
+            return ft_substr(envp[i], len_var_name, len_total - len_var_name);
         }
     }
-    return NULL;
+    return ft_strdup("");
 }
 
 char *replace_char_with_expandable(char *original, char char_to_expand, char *expandable)
@@ -111,28 +112,63 @@ void expand_home_dir (t_token *list_elem, char **envp)
     //printf("temp is %s\n", temp);        
 }
 
-//TODO: var_name ohne klammern am ende
+/*
+*  helper for the case that the $ is not in the beginning but in the middle of a token
+*  in that case we have to strjoin the first string until the $ (excluding) with the var value
+*
+*/
+char *return_helper(char *str, char *temp)
+{
+    char *dollar_position;
+    int length_to_keep;
+    
+    dollar_position = ft_strchr(str, '$');
+    length_to_keep = dollar_position - str;
+    
+    //possible todo das nicht in einer zeile
+    return ft_strjoin(ft_substr(str, 0, length_to_keep), temp);
+}
+
+//TODO: var_name ohne klammern am ende (???)
+/*  replaces the first occurrence of $ with the value
+*/
 char *replace_var_with_content(char *str, char **envp)
 {
     int i;
-    char *var_name;
+    char *var_name_without_istgleich;
+    char *var_name_with_istgleich;
     char *temp;
-    
+
+    temp = ft_strdup("");
+    if (!temp)
+        return (NULL);
     i = -1;
     while (str[++i])
     {
         if (str[i] == '$')
         {
-            var_name = ft_substr(str, 1, ft_strlen(str + 1));
-            //if (!var_name)
-            //error
-            printf("var_name is %s\n", var_name);
+            var_name_without_istgleich = ft_substr(str, i + 1, ft_strlen(str + 1));
+            if (!var_name_without_istgleich)
+                return (temp);
+            var_name_with_istgleich = ft_strjoin(var_name_without_istgleich, "=");
+            if (!var_name_with_istgleich)
+            {
+                free(var_name_without_istgleich);
+                return (temp);
+            }
+            printf("var_name is %s\n", var_name_with_istgleich);
             //for variables in envp
-            temp = extract_var_from_envp(var_name, envp);
+            free(temp);
+            temp = extract_var_from_envp(var_name_with_istgleich, envp);
+            free(var_name_without_istgleich);
+            free(var_name_with_istgleich);
             break;
         }
     }
-    return temp;
+    if (str[0] == '$')
+        return (temp);
+    else
+        return return_helper(str, temp);
 }
 
 //replaces all occurences of a variable with the dedicated value
@@ -141,22 +177,26 @@ void expand_variable (t_token *list_elem, char **envp)
 {
     int i;
     char *temp;
+    char *expanded;
 
-    i = -1;
-    while(list_elem->str[++i])
+    i = 0;
+    while(list_elem->str && list_elem->str[i])
     {
         if (list_elem->str[i] == '$')
         {
             temp = ft_strdup(list_elem->str);
             //ERROR
             free(list_elem->str);
-            
-            list_elem->str = replace_var_with_content(temp, envp);
+
+            expanded = replace_var_with_content(temp, envp);
+            free(temp);
+            list_elem->str = expanded;
             //if (!list_elem->status)
             //error
             i = 0;
+            continue;
         }
-            
+        i++;   
     }
 }
 

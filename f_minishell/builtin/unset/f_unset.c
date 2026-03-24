29275e56a9520163a3510297490e79c354045928
fsitter42 <fsitter@42vienna.com>
2026-03-24 15:56:25 +0100

    suw

--- f_minishell/builtin/unset/f_unset.c
+++ f_minishell/builtin/unset/f_unset.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 01:09:18 by fsitter           #+#    #+#             */
-/*   Updated: 2026/03/24 15:38:25 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/03/24 15:54:06 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	f_unset(t_data *data, char **args)
 	ret = EXIT_SUCCESS;
 	if (!data || !args || !args[0])
 		return (EXIT_FAILURE);
-    if (!args[1])
+	if (!args[1])
 		return (EXIT_SUCCESS);
 	else
-    {
+	{
 		ret = f_lst_del_one(&data->env->envp_lst, args[1]);
-        ret = f_update_envp(data->env);
-    }
-    return (ret);
+		ret = f_update_envp(data->env);
+	}
+	return (ret);
 }
 
 int	f_lst_del_one(t_envl **list, char *key)

94208f3be445a276978773f4e72f5c68d9e1a82f
fsitter42 <fsitter@42vienna.com>
2026-03-25 16:20:50 +0100

    suwi

--- f_minishell/execution/pathfinder/f_pathfinder.c
+++ f_minishell/execution/pathfinder/f_pathfinder.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 11:59:42 by fsitter           #+#    #+#             */
-/*   Updated: 2026/03/25 16:02:57 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/03/25 16:20:37 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,8 @@ static char	*f_join_cmd_to_path(char *dir, char *cmd)
 	if (!tmp)
 		return (NULL);
 	full = ft_strjoin(tmp, cmd);
+	// free (full);
+	// full = NULL;
 	if (!full)
 		return (free(tmp), NULL);
 	free(tmp);

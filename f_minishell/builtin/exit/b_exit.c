2488c49b17237054ae3c658613ce75599c66a37a
Stefan Lambert <slambert@student.42vienna.com>
2026-03-25 18:01:09 +0100

    refined exit builtin

--- f_minishell/builtin/exit/b_exit.c
+++ f_minishell/builtin/exit/b_exit.c
@@ -6,15 +6,18 @@
 /*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 13:22:51 by slambert          #+#    #+#             */
-/*   Updated: 2026/03/25 17:42:11 by slambert         ###   ########.fr       */
+/*   Updated: 2026/03/25 18:00:23 by slambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../minishell.h"
 
+//TODO exit 24 asd should print minishell: exit: too many arguments
+//and NOT actually exit
 static void	write_exit_error_and_free(char *str, t_data *data, int *saved_fds,
 		int is_num)
 {
+	ft_putendl_fd("exit", STDOUT_FILENO);
 	if (!is_num)
 	{
 		ft_putendl_fd_no_newline("minishell: exit: ", STDERR_FILENO);
@@ -36,14 +39,19 @@ void	b_exit(t_data *data, char **args, int *saved_fds)
 	exit_no = 0;
 	if (args[1])
 	{
-		i = -1;
-		while (args[1][++i])
+		i = 0;
+		if (args[1][i] == '+' || args[1][i] == '-')
+			i++;
+		if (args[1][i] == '\0')
+			is_num = 0;
+		while (is_num && args[1][i])
 		{
-			if (!ft_isdigit(args[1][i]))
+			if (!ft_isdigit((unsigned char)args[1][i]))
 			{
 				is_num = 0;
 				break ;
 			}
+			i++;
 		}
 		if (is_num == 0)
 			exit_no = 2;

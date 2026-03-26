388b3fb7a759411b2485bcc466708ef041209ecb
Stefan Lambert <slambert@student.42vienna.com>
2026-03-26 16:45:22 +0100

    minishell.c refactor

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
+/*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 16:52:01 by slambert          #+#    #+#             */
-/*   Updated: 2026/03/26 16:43:11 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/03/26 16:47:00 by slambert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 int			g_last_exit_code;
 
-
-int f_is_syntax_valid(t_data *data);
+int			f_is_syntax_valid(t_data *data);
 /*
 	TODO:
 	- handle exit status (should work now, test)
@@ -28,7 +27,7 @@ int	is_token_list_empty(t_token *token_list)
 {
 	if (!token_list || !token_list->next)
 	{
-		//printf("Token list is empty\n");
+		// printf("Token list is empty\n");
 		return (1);
 	}
 	return (0);
@@ -53,20 +52,53 @@ int	handle_delimiter(t_token *token_list)
 	return (RET_OK);
 }
 
-/* TODO think about return value. does 1 always mean something went wrong 
- * (close minishell)  * or are there cases where nothing went wrong and we 
- * just do nothing and give the input back to the user? 
+int	hsl_helper(t_token *token_list, t_cmd *cmd_list, t_data *data)
+{
+	int ret;
+
+	ret = RET_OK;
+	if (!is_token_list_empty(token_list))
+	{
+		cmd_list = create_command_list(token_list->next);
+		if (!cmd_list)
+			return (cleanup_token_list(token_list), ERROR_HARD);
+		cleanup_token_list(token_list);
+		data->cmds = cmd_list;
+		ret = f_is_syntax_valid(data);
+		if (ret != RET_OK)
+		{
+			cleanup_command_list(cmd_list);
+			data->cmds = NULL;
+			return (ret);
+		}
+		ret = eggsecute(data); // frido geändert
+	}
+	else
+		cleanup_token_list(token_list);
+	// TODO wir müssen überall data->should_exit auf 1 setzen in den builtins
+	// alternativ ret -1 oder 600
+	// oder wir ändern exec_ret auch auf ERROR_HARD (dann müssten wir return
+	// von f_exec_builtin und f_pipeline_wrapper ändern und in allen dahinter
+	// liegenden funktionen)
+	if (ret == 1 && data->should_exit == 1)
+		return (ERROR_HARD);
+	cleanup_command_list(cmd_list);
+	data->cmds = NULL;
+	return (RET_OK);
+}
+
+/* TODO think about return value. does 1 always mean something went wrong
+ * (close minishell)  * or are there cases where nothing went wrong and we
+ * just do nothing and give the input back to the user?
  * i think we need 3 states, OK, ERROR_RECOVERABLE (user gets input back)
  * and ERROR_FATAL (minishell exits and everything gets cleaned up)
  */
 int	handle_single_line(char *line, char **envp, t_data *data)
 {
-	int	exec_ret;
-	int	ret;
+	int		ret;
 	t_token	*token_list;
 	t_cmd	*cmd_list;
 
-	exec_ret = RET_OK;
 	cmd_list = NULL;
 	token_list = tokenizer(line);
 	if (!token_list)
@@ -80,31 +112,7 @@ int	handle_single_line(char *line, char **envp, t_data *data)
 	ret = word_split(token_list);
 	if (ret != RET_OK)
 		return (cleanup_token_list(token_list), ret);
-	if (!is_token_list_empty(token_list))
-	{
-    	cmd_list = create_command_list(token_list->next);
-    	if (!cmd_list)
-	    	    return (cleanup_token_list(token_list), ERROR_HARD);	
-    	cleanup_token_list(token_list);
-    	data->cmds = cmd_list; 	
-	    	ret = f_is_syntax_valid(data);
-	    	if (ret != RET_OK)
-    	{
-        	cleanup_command_list(cmd_list);
-        	data->cmds = NULL;
-	        	return (ret);
-    	}
-	    	exec_ret = eggsecute(data); //frido geändert
-	}
-	else
-		cleanup_token_list(token_list);
-	//TODO wir müssen überall data->should_exit auf 1 setzen in den builtins
-	//alternativ ret -1 oder 600
-	if (exec_ret == 1 && data->should_exit == 1)
-		return (ERROR_HARD);
-	cleanup_command_list(cmd_list);
-	data->cmds = NULL;
-	return (RET_OK);
+	return hsl_helper(token_list, cmd_list, data);
 }
 
 /* this is the default mode in where the users enters stuff
@@ -147,7 +155,8 @@ static void	cleanup_split_result(char **strs, int start)
 
 /* the difference here is that we have one string that is given to
  *  minishell, no interactions with the user. all lines have to be separated
- *  by a semicolon (;) and will be executed one afprintf("hello nur\n");ter each other.
+
+	*  by a semicolon (;) and will be executed one afprintf("hello nur\n");ter each other.
 
 	*  note: if we want to use a $ sign (var) we have to explicitly escape that character (\\ before §).
 
@@ -180,24 +189,24 @@ void	debug_mode(char *input, char **envp, t_data *data)
 	}
 }
 
-//TODO $EMPTY echo hi does not print anything
+// TODO $EMPTY echo hi does not print anything
 void	sfbf_free_all(t_data *data)
 {
-	int i;
-	
+	int	i;
+
 	if (!data)
 		return ;
 	if (data->env->envp_lst)
-		f_free_env_list(data->env->envp_lst); //vll nmoch freen zusätzliuch
+		f_free_env_list(data->env->envp_lst); // vll nmoch freen zusätzliuch
 	if (data->env->envp_updated)
 		f_free_envp(data->env->envp_updated);
 	// if (data->env->envp_ori)
-	// 	free(data->env->envp_ori);	
+	// 	free(data->env->envp_ori);
 	free(data->env);
 	i = 0;
 	while (data->strs && data->strs[i])
 	{
-		free (data->strs[i]);
+		free(data->strs[i]);
 		i++;
 	}
 	if (data->strs)
@@ -212,8 +221,8 @@ void	sfbf_free_all(t_data *data)
 t_data	*sfbf_init_all(char **envp)
 {
 	t_data	*data;
-	char **args;
-	
+	char	**args;
+
 	data = ft_calloc(sizeof(t_data), 1);
 	if (!data)
 		return (NULL);
@@ -242,11 +251,10 @@ int	main(int argc, char **argv, char **envp)
 		return (printf("wrong syntax - argc not 1 or 3\n"), 1);
 	if (argc == 1)
 	{
-		// hier env init
 		data = sfbf_init_all(envp);
 		if (!data)
 			return (1);
-		normal_mode(argc, argv, envp, data);	
+		normal_mode(argc, argv, envp, data);
 	}
 	else
 	{
@@ -260,21 +268,22 @@ int	main(int argc, char **argv, char **envp)
 	sfbf_free_all(data);
 }
 
-int f_is_syntax_valid(t_data *data)
+int	f_is_syntax_valid(t_data *data)
 {
-    t_cmd *tmp;
+	t_cmd	*tmp;
 
-    tmp = data->cmds;
-    while (tmp)
-    {
-        if (!tmp->cmd && !tmp->redirs)
-        {
-            ft_putstr_fd("minishell: syntax error near unexpected token '|'\n", 2);
-            data->last_exit_code = 258;
+	tmp = data->cmds;
+	while (tmp)
+	{
+		if (!tmp->cmd && !tmp->redirs)
+		{
+			ft_putstr_fd("minishell: syntax error near unexpected token '|'\n",
+				2);
+			data->last_exit_code = 258;
 			return (ERROR_SOFT);
-        }
-        tmp = tmp->next;
-    }
+		}
+		tmp = tmp->next;
+	}
 	return (RET_OK);
 }
 

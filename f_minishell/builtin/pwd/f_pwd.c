92b5546a48d3f4c57c1369073025dd9da700190d
fsitter42 <fsitter@42vienna.com>
2026-03-28 11:52:06 +0100

    updated pwd function for malloc error

--- f_minishell/builtin/pwd/f_pwd.c
+++ f_minishell/builtin/pwd/f_pwd.c
@@ -3,22 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   f_pwd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 14:49:53 by fsitter           #+#    #+#             */
-/*   Updated: 2026/03/22 16:00:16 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/03/28 11:51:20 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../minishell.h"
 
-int	f_pwd(void)
+int	f_pwd(t_data *data)
 {
 	char	*cwd;
 
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
+	{
+		data->should_exit = 1;
 		return (EXIT_FAILURE);
+	}
 	ft_putendl_fd(cwd, 1);
 	free(cwd);
 	return (EXIT_SUCCESS);

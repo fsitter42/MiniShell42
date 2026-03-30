5b04eca5051a3a79d7d37c16b487b87f2a2d4147
fsitter42 <fsitter@42vienna.com>
2026-03-30 10:51:10 +0200

    made cd norminettconform and memsafe

--- f_minishell/builtin/cd/f_cd.c
+++ f_minishell/builtin/cd/f_cd.c
@@ -6,40 +6,116 @@
 /*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 14:49:53 by fsitter           #+#    #+#             */
-/*   Updated: 2026/03/28 12:36:46 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/03/30 10:50:27 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../minishell.h"
 
+static int	f_path_exporter(t_cd *cd, t_data *data);
+static void	f_deinit_cd(t_cd *cd);
+static int	f_init_cd(t_cd *cd, t_data *data, char **args);
+
 int	f_cd(t_data *data, char **args)
 {
-	char	*old_pwd;
-	char	*new_pwd;
-	char	*export_str;
-	char	*target;
+	t_cd	cd;
 
+	if (f_init_cd(&cd, data, args) != EXIT_SUCCESS)
+		return (f_deinit_cd(&cd), EXIT_FAILURE);
+	if (chdir(cd.target) == -1)
+		return (f_print_error(cd.target, strerror(errno)), free(cd.old_pwd), 1);
+	cd.new_pwd = getcwd(NULL, 0);
+	if (!cd.new_pwd)
+		return (data->should_exit = 1, f_deinit_cd(&cd), EXIT_FAILURE);
+	if (f_path_exporter)
+		return (f_deinit_cd(&cd), EXIT_FAILURE);
+	return (f_deinit_cd(&cd), EXIT_FAILURE);
+}
+
+static int	f_path_exporter(t_cd *cd, t_data *data)
+{
+	cd->export_str = ft_strjoin("OLDPWD=", cd->old_pwd);
+	if (!cd->export_str)
+		return (data->should_exit = 1, EXIT_FAILURE);
+	if (f_export(data, (char *[]){"export", cd->export_str, NULL}) == 1)
+		return (EXIT_FAILURE);
+	free(cd->export_str);
+	cd->export_str = ft_strjoin("PWD=", cd->new_pwd);
+	if (!cd->export_str)
+		return (data->should_exit = 1, EXIT_FAILURE);
+	if (f_export(data, (char *[]){"export", cd->export_str, NULL}) == 1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+static void	f_deinit_cd(t_cd *cd)
+{
+	if (cd->export_str)
+		free(cd->export_str);
+	if (cd->old_pwd)
+		free(cd->old_pwd);
+	if (cd->new_pwd)
+		free(cd->new_pwd);
+	if (cd->has_target && cd->target)
+		free(cd->target);
+	return ;
+}
+
+static int	f_init_cd(t_cd *cd, t_data *data, char **args)
+{
+	cd->old_pwd = NULL;
+	cd->new_pwd = NULL;
+	cd->export_str = NULL;
+	cd->target = NULL;
+	cd->has_target = 0;
 	if (args[2] != NULL)
 		return (f_print_error("cd", "too many arguments"), EXIT_FAILURE);
 	if (args[1])
-		target = args[1];
+	{
+		cd->target = args[1];
+		cd->has_target = 1;
+	}
 	else
-		target = f_get_env_val(data->env->envp_lst, "HOME", data);
-	if (!target)
+		cd->target = f_get_env_val(data->env->envp_lst, "HOME", data);
+	if (!cd->target)
 		return (EXIT_FAILURE);
-	old_pwd = getcwd(NULL, 0);
-	if (chdir(target) == -1)
-		return (f_print_error(target, strerror(errno)), free(old_pwd),
-			EXIT_FAILURE);
-	new_pwd = getcwd(NULL, 0);
-	export_str = ft_strjoin("OLDPWD=", old_pwd);
-	if (f_export(data, (char *[]){"export", export_str, NULL}) == EXIT_FAILURE)
-		return (free(export_str), free(old_pwd), free(new_pwd), EXIT_FAILURE);
-	free(export_str);
-	export_str = ft_strjoin("PWD=", new_pwd);
-	if (f_export(data, (char *[]){"export", export_str, NULL}) == EXIT_FAILURE)
-		return (free(export_str), free(old_pwd), free(new_pwd), EXIT_FAILURE);
-	return (free(export_str), free(old_pwd), free(new_pwd), EXIT_SUCCESS);
+	cd->old_pwd = getcwd(NULL, 0);
+	if (!cd->old_pwd)
+	{
+		data->should_exit = 1;
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
-//TODOF getcwd protecten
+// int	f_cd(t_data *data, char **args)
+// {
+// 	char	*old_pwd;
+// 	char	*new_pwd;
+// 	char	*export_str;
+// 	char	*target;
+
+// 	if (args[2] != NULL)
+// 		return (f_print_error("cd", "too many arguments"), EXIT_FAILURE);
+// 	if (args[1])
+// 		target = args[1];
+// 	else
+// 		target = f_get_env_val(data->env->envp_lst, "HOME", data);
+// 	if (!target)
+// 		return (EXIT_FAILURE);
+// 	old_pwd = getcwd(NULL, 0);
+// 	if (chdir(target) == -1)
+// 		return (f_print_error(target, strerror(errno)), free(old_pwd),
+// 			EXIT_FAILURE);
+// 	new_pwd = getcwd(NULL, 0);
+// 	export_str = ft_strjoin("OLDPWD=", old_pwd);
+// 	if (f_export(data, (char *[]){"export", export_str, NULL}) == EXIT_FAILURE)
+// 		return (free(export_str), free(old_pwd), free(new_pwd), EXIT_FAILURE);
+// 	free(export_str);
+// 	export_str = ft_strjoin("PWD=", new_pwd);
+// 	if (f_export(data, (char *[]){"export", export_str, NULL}) == EXIT_FAILURE)
+// 		return (free(export_str), free(old_pwd), free(new_pwd), EXIT_FAILURE);
+// 	return (free(export_str), free(old_pwd), free(new_pwd), EXIT_SUCCESS);
+// }
+
+// TODOF getcwd protecten
--- f_minishell/builtin/cd/f_cd.h
+++ f_minishell/builtin/cd/f_cd.h
@@ -6,15 +6,25 @@
 /*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 15:00:03 by fsitter           #+#    #+#             */
-/*   Updated: 2026/03/11 15:08:49 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/03/30 10:31:21 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef CD_H
 # define CD_H
 
+typedef struct s_cd t_cd;
+
+typedef struct s_cd
+{
+	char	*old_pwd;
+	char	*new_pwd;
+	char	*export_str;
+	char	*target;
+    int     has_target;
+}			t_cd;
+
 // f_cd.c
-int	f_cd(t_data *data, char **args);
+int			f_cd(t_data *data, char **args);
 
 #endif

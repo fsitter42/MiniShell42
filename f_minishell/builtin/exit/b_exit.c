40abcd2f86263c15ba91677a8d3e82206f2687dc
Your Name <you@example.com>
2026-04-18 01:04:19 +0200

    exit

--- f_minishell/builtin/exit/b_exit.c
+++ f_minishell/builtin/exit/b_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   b_exit.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slambert <slambert@student.42vienna.com    +#+  +:+       +#+        */
+/*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 13:22:51 by slambert          #+#    #+#             */
-/*   Updated: 2026/04/17 14:40:35 by slambert         ###   ########.fr       */
+/*   Updated: 2026/04/18 01:03:09 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int	set_is_num(char **args)
 
 	is_num = 1;
 	i = 0;
+	while (args[1][i] == ' ' || args[1][i] == '\t')
+		i++;
 	if (args[1][i] == '+' || args[1][i] == '-')
 		i++;
 	if (args[1][i] == '\0')
@@ -35,7 +37,10 @@ int	set_is_num(char **args)
 	{
 		if (!ft_isdigit(args[1][i]))
 		{
-			is_num = 0;
+			while (args[1][i] == ' ' || args[1][i] == '\t')
+				i++;
+			if (args[1][i] != '\0')
+				is_num = 0;
 			break ;
 		}
 		i++;
@@ -52,8 +57,8 @@ static void	write_error_and_set_exit_code(t_data *data, t_cmd *cmd)
 
 void	too_many_args_handler(t_data *data, t_cmd *cmd)
 {
-	int is_num;
-	
+	int	is_num;
+
 	is_num = set_is_num(cmd->args);
 	if (is_num == 0)
 	{
@@ -83,22 +88,22 @@ void	too_many_args_handler(t_data *data, t_cmd *cmd)
 // {
 // 	long long num;
 // 	int i;
-	
+
 // 	i = 0;
 // 	if ()
 // 	if (...)
-// 		return 1;
-// 	return 0;
+// 		return (1);
+// 	return (0);
 // }
 
 // static int is_ll_underflow (char *arg)
 // {
 // 	if (...)
-// 		return 1;
-// 	return 0;
+// 		return (1);
+// 	return (0);
 // }
 
-//TODO return was gscheids
+// TODO return was gscheids
 // long long create_exit_code (char *arg, t_data *data, t_cmd* cmd)
 // {
 // 	if (is_ll_overflow(arg) || is_ll_underflow(arg))
@@ -106,19 +111,113 @@ void	too_many_args_handler(t_data *data, t_cmd *cmd)
 // 		//on overflow we have to write bash: exit: 555555555555555555555555555555555555555555555555555: numeric argument required
 // 		//AND exit with status code 2
 // 		write_error_and_set_exit_code(data, cmd);
-// 		return 2;
+// 		return (2);
 // 	}
 // 	else
 // 		return (unsigned char)ft_atoi(arg);
 // }
 
+// void	b_exit(t_data *data, t_cmd *cmd)
+// {
+// 	int	exit_no;
+// 	int	is_num;
+
+// 	is_num = 0;
+// 	exit_no = 0;
+// 	if (cmd->args[1] && cmd->args[2])
+// 	{
+// 		too_many_args_handler(data, cmd);
+// 		return ;
+// 	}
+// 	if (cmd->is_first)
+// 		ft_putendl_fd("exit", STDOUT_FILENO);
+// 	if (cmd->args[1])
+// 	{
+// 		is_num = set_is_num(cmd->args);
+// 		if (is_num == 0)
+// 		{
+// 			write_error_and_set_exit_code(data, cmd);
+// 			return ;
+// 		}
+// 		else
+// 			exit_no = (unsigned char)ft_atoi(cmd->args[1]);
+// 		// exit_no = create_exit_code(cmd->args[1], data, cmd);
+// 	}
+// 	data->last_exit_code = exit_no;
+// 	data->should_exit = 1;
+// }
+
+////////////////////////////////////////////
+
+static int	check_digit(long long *num, int neg, long long limit, char c)
+{
+	if (!neg && *num > (limit - (c - '0')) / 10)
+		return (1);
+	if (neg && *num < (limit + (c - '0')) / 10)
+		return (1);
+	if (!neg)
+		*num = *num * 10 + (c - '0');
+	else
+		*num = *num * 10 - (c - '0');
+	return (0);
+}
+
+static int	is_overflow(char *arg)
+{
+	long long	num;
+	long long	limit;
+	int			neg;
+	int			i;
+
+	i = 0;
+	neg = 0;
+	num = 0;
+	while (arg[i] == ' ' || arg[i] == '\t')
+		i++;
+	if (arg[i] == '+' || arg[i] == '-')
+		neg = (arg[i++] == '-');
+	if (neg)
+		limit = LLONG_MIN;
+	else
+		limit = LLONG_MAX;
+	while (ft_isdigit(arg[i]))
+	{
+		if (check_digit(&num, neg, limit, arg[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static long long	f_atoll(char *arg)
+{
+	long long	num;
+	int			neg;
+	int			i;
+
+	i = 0;
+	neg = 0;
+	num = 0;
+	while (arg[i] == ' ' || arg[i] == '\t')
+		i++;
+	if (arg[i] == '+' || arg[i] == '-')
+		neg = (arg[i++] == '-');
+	while (ft_isdigit(arg[i]))
+	{
+		if (neg)
+			num = num * 10 - (arg[i] - '0');
+		else
+			num = num * 10 + (arg[i] - '0');
+		i++;
+	}
+	return (num);
+}
+
 void	b_exit(t_data *data, t_cmd *cmd)
 {
-	int			exit_no;
-	int			is_num;
+	int	is_num;
 
 	is_num = 0;
-	exit_no = 0;
 	if (cmd->args[1] && cmd->args[2])
 	{
 		too_many_args_handler(data, cmd);
@@ -128,17 +227,15 @@ void	b_exit(t_data *data, t_cmd *cmd)
 		ft_putendl_fd("exit", STDOUT_FILENO);
 	if (cmd->args[1])
 	{
-		
 		is_num = set_is_num(cmd->args);
-		if (is_num == 0)
+		if (!is_num || is_overflow(cmd->args[1]))
 		{
 			write_error_and_set_exit_code(data, cmd);
 			return ;
 		}
-		else
-			exit_no = (unsigned char)ft_atoi(cmd->args[1]);
-			//exit_no = create_exit_code(cmd->args[1], data, cmd);
+		data->last_exit_code = (unsigned char)f_atoll(cmd->args[1]);
 	}
-	data->last_exit_code = exit_no;
 	data->should_exit = 1;
 }
+
+// TODO F 2. file norm

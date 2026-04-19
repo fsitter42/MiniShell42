c1a6602fef8ffaeefacf1a30745b5264c6738c07
Your Name <you@example.com>
2026-04-19 03:20:30 +0200

    tests

--- f_minishell/execution/f_exe.c
+++ f_minishell/execution/f_exe.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 14:01:22 by fsitter           #+#    #+#             */
-/*   Updated: 2026/04/19 02:51:54 by fsitter          ###   ########.fr       */
+/*   Updated: 2026/04/19 03:17:32 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,6 @@ static void	f_child_process(t_data *data, t_cmd *cmd, int *prev_fd,
 				int *pipe_fd);
 static void	f_exec_builtin_child(t_cmd *cmd, t_data *data);
 static void	f_parent_cleanup(t_cmd *cmd, int *prev_fd, int *pipe_fd);
-
-int mypipe(int fd[2])
-{
-	(void)fd[0];
-	(void)fd[1];
-	return (-1);
-}
-
-int myfork()
-{
-	return (-1);
-}
 
 int	f_exec_pipeline(t_data *data, t_cmd *cmd, int pipe_fd[2])
 {
